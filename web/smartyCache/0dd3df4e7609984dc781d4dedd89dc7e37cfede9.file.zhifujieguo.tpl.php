48574f13b4541958840531e6612d2987345f3571
xbruence <372260652@qq.com>
2016-09-24 13:31:31 +0800

    a

---
+++ web/smartyCache/0dd3df4e7609984dc781d4dedd89dc7e37cfede9.file.zhifujieguo.tpl.php
@@ -0,0 +1,18 @@
+<?php /* Smarty version Smarty-3.0-RC2, created on 2016-09-24 13:26:35
+         compiled from "/Users/Lev/Sites/HuanYaoBang/web/Lib/Homepage/Tpl/Homepage/zhifujieguo.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:124205793057e60e8b12d9d2-13776326%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0dd3df4e7609984dc781d4dedd89dc7e37cfede9' => 
+    array (
+      0 => '/Users/Lev/Sites/HuanYaoBang/web/Lib/Homepage/Tpl/Homepage/zhifujieguo.tpl',
+      1 => 1474461204,
+    ),
+  ),
+  'nocache_hash' => '124205793057e60e8b12d9d2-13776326',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
